144209e3dac6c4bc93532e84bdc0aa9047af91d6
LuisPaulin <laps_paulin@hotmail.com>
2020-11-23 00:47:18 -0600

    Endpoint to get all pokemons

---
+++ router/router.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/luispaulin/api-challenge/interface/controller"
+)
+
+// NewRouter to register endpoints
+func NewRouter(e *echo.Echo, c controller.AppController) *echo.Echo {
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/pokemons", func(context echo.Context) error { return c.GetPokemons(context) })
+
+	return e
+}
