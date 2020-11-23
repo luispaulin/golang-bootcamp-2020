954210c3e070445261c29940031b835990445aba
LuisPaulin <laps_paulin@hotmail.com>
2020-11-23 00:59:06 -0600

    Comments in model

--- domain/model/pokemon.go
+++ domain/model/pokemon.go
@@ -1,9 +1,10 @@
 package model
 
 // Pokemon model
+// TODO: Can I use csv notations in model?
 type Pokemon struct {
-	ID    uint16
-	Name  string
-	Type1 string
-	Type2 string
+	ID    uint16 `csv:"id"`
+	Name  string `csv:"name"`
+	Type1 string `csv:"type1"`
+	Type2 string `csv:"type2"`
 }
